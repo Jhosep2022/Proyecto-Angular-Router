35d7f710cd8c87fbdaf0cdfb6be235c06122a91b
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2024-05-11 18:06:26 -0400

    creacion de proyecto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import ContactoComponent from './dashboard/pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'contactillo',
+        title: 'Contacto',
+        loadComponent: () => ContactoComponent,
+      },
+      {
+        path: 'perfil',
+        title: 'Perfil',
+        loadComponent: () => import('./dashboard/pages/perfil/perfil.component'),
+      },
+      {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./dashboard/pages/home/home.component'),
+      },
+      {
+        path: '',
+        redirectTo: 'control-flow',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
